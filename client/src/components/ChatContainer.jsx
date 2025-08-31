be26b304a462a13e9b0420fd45978f2e3203c9f9
Tushar <chauhantushar6706@gmail.com>
2025-08-31 22:19:14 +0530

    chatcontainer

--- client/src/components/ChatContainer.jsx
+++ client/src/components/ChatContainer.jsx
@@ -1,19 +1,24 @@
 import React from 'react'
 import assets from '../assets/assets'
 
-const ChatContainer = ({selectedUser , setSelectedUser}) => {
-  return (
-    <div className='h-full overflow-scroll relative backdrop-blur-lg'>
-      <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
-        <img src={assets.profile_martin} alt=""  className='w-8 rounded-full'/>
-        <p className='flex-1 text-lg flex items-center gap-2 text-white '> Martin Johnson
-            <span className='text-green-500 text-xs block rounded-full'>Online</span>
-        </p>
-        <img onClick={()=>setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md-hidden max-w-7' />
-        <img src={assets.help_icon} alt="" className='max-md:hidden max-w-7 ' />
-      </div>
-    </div>
-  )
+const ChatContainer = ({ selectedUser, setSelectedUser }) => {
+    return selectedUser ? (
+        <div className='h-full overflow-scroll relative backdrop-blur-lg'>
+            <div className='flex items-center gap-3 py-3 mx-4 border-b border-stone-500'>
+                <img src={assets.profile_martin} alt="" className='w-8 rounded-full' />
+                <p className='flex-1 text-lg flex items-center gap-2 text-white '> Martin Johnson
+                    <span className='text-green-500 text-xs block rounded-full'>Online</span>
+                </p>
+                <img onClick={() => setSelectedUser(null)} src={assets.arrow_icon} alt="" className='md-hidden max-w-7' />
+                <img src={assets.help_icon} alt="" className='max-md:hidden max-w-7 ' />
+            </div>
+        </div>
+    ) : (
+        <div className='flex-col gap-2 text-gray-500 bg-white/10 max-md:hidden  flex items-center justify-center'>
+            <img src={assets.logo_icon} alt="" className='max-w-16' />
+            <p className='text-gray-500'>Chat anytime , anywhere</p>
+        </div>
+    )
 }
 
 export default ChatContainer
