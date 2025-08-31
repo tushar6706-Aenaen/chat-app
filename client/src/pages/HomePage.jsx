a3884988469c309a862f114851a3ed218144d195
Tushar <chauhantushar6706@gmail.com>
2025-08-31 22:13:51 +0530

    sidebar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -9,11 +9,11 @@ const HomePage = () => {
 
 
   return (
-    <div className='border w-full h-screen sm:px-[15%] sm:py-[5%] '>
-      <div className={`background-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2  '}`}>
-        <Sidebar/>
-        <ChatContainer/>
-        <RightSidebar/>
+    <div className='border w-full h-screen  '>
+      <div className={`background-blur-xl border-2 border-gray-600 rounded-2xl overflow-hidden h-screen grid grid-cols-1 relative ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2  '}`}>
+        <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+        <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
       </div>
     </div>
   )
